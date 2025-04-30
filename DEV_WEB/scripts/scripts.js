8c12c9800dc464d87570473ef3dea09769beb4b4
Gabriel Santos <gabriel.santos@bit7forge.com>
2025-04-30 14:57:35 -0300

    remoção do modo dark, responsividade da pagina home

--- DEV_WEB/scripts/scripts.js
+++ DEV_WEB/scripts/scripts.js
@@ -1,19 +1,3 @@
-let trilho = document.getElementById("trilho");
-let body = document.querySelector("body");
-let header = document.querySelector("header");
-let logo = document.querySelector("logoHome");
-let footer = document.querySelector("footer");
-let text = document.querySelector("text");
-
-trilho.addEventListener("click", () => {
-  trilho.classList.toggle("dark");
-  body.classList.toggle("dark");
-  header.classList.toggle("dark");
-  logo.classList.toggle("dark");
-  text.classList.toggle("dark");
-  footer.classList.toggle("dark");
-});
-
 // Função para alternar o menu
 const hamburger = document.getElementById("hamburger");
 const menu = document.getElementById("menu");
